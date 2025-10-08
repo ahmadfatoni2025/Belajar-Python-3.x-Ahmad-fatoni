2ffe82ffc59da19c78aad54859aa232e0beeba74
toni <ahfatoni909@gmail.com>
2025-10-08 21:24:23 +0700

    versi 1

---
+++ Eksperimental/tugasAlgoritma copy.py
@@ -0,0 +1,23 @@
+# def x(kresek):
+#     if kresek >= 85:
+#         return f"Nilai anda adalah {kresek} (A)"
+#     elif kresek >= 70:
+#         return f"Nilai anda adalah {kresek} (B)"
+#     elif kresek >= 55:
+#         return f"Nilai anda adalah {kresek} (C)"
+#     else:
+#         return f"Nilai anda adalah {kresek} (D)"
+
+# masukanNilai = int(input("Masukan Nilainya = "))
+# print(x(masukanNilai))
+
+a = int(input("masukan nilai:"))
+
+if a >= 85:
+    print("Nilai koe adalah(A)")
+elif a >= 70:
+    print("Nilai koe adalah(B)")
+elif a >= 70:
+    print("Nilai koe adalah(C)")
+else:
+    print("Nilai koe adalah(D)")
